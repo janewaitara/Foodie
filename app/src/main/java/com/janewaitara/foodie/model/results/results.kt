06902f8aed32b322e1a158ceb7a187deb971d766
janewaitara <janewaitara99@gmail.com>
2020-08-12 18:59:38 +0300

    Added a Result class to handle networking error

--- app/src/main/java/com/janewaitara/foodie/model/results/results.kt
+++ app/src/main/java/com/janewaitara/foodie/model/results/results.kt
@@ -1,2 +1,7 @@
 package com.janewaitara.foodie.model.results
 
+sealed class Result <out T: Any>
+
+data class Success <out T: Any>(val data: T): Result<T>()
+
+data class Failure (val error: Throwable): Result<Nothing>()
