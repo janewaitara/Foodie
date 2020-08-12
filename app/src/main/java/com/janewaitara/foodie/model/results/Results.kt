130695a1496e438f12fa1b32187d5525aa7464ec
janewaitara <janewaitara99@gmail.com>
2020-08-12 19:55:52 +0300

    Added a networking module for dependency injection

--- app/src/main/java/com/janewaitara/foodie/model/results/Results.kt
+++ app/src/main/java/com/janewaitara/foodie/model/results/Results.kt
@@ -6,4 +6,4 @@ data class Success <out T: Any>(val data: T): Result<T>()
 
 data class Failure (val error: Throwable): Result<Nothing>()
 
- class Loading<T> : Result<T>()
+class Loading<T> : Result<T>()
