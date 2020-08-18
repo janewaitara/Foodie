bb5a89af06605e3811df734adca1d2a9a34e21d6
janewaitara <janewaitara99@gmail.com>
2020-08-18 23:13:54 +0300

    Added logic to display favorite recipes

--- app/src/main/java/com/janewaitara/foodie/ui/favoriteRecipes/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/janewaitara/foodie/ui/favoriteRecipes/FavoriteRecipesFragment.kt
@@ -5,9 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.janewaitara.foodie.R
+import com.janewaitara.foodie.model.data.FavoriteRecipe
+import com.janewaitara.foodie.utils.isVisible
+import kotlinx.android.synthetic.main.fragment_favorite_recipes.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class FavoriteRecipesFragment : Fragment() {
+class FavoriteRecipesFragment : Fragment() , FavoriteRecipeAdapter.FavRecipeClickListener {
+
+    private val favRecipeViewModel: FavoriteRecipeViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +24,65 @@ class FavoriteRecipesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpData()
+    }
+
+    private fun setUpData() {
+        favRecipeViewModel.getFavoriteRecipe()
+
+        favRecipeViewModel.getFavRecipeViewState().observe(viewLifecycleOwner, Observer{ favRecipeViewState ->
+
+            favRecipeViewState?.let {favoriteRecipeViewState ->
+                onDetailsViewStateChanged(favoriteRecipeViewState)
+            }
+        })
+    }
+
+    private fun onDetailsViewStateChanged(favoriteRecipeViewState: FavoriteRecipeViewState) {
+        when(favoriteRecipeViewState){
+            FavRecipeLoading -> showLoading(true)
+            is FavRecipeSuccess -> {
+               displayFavoriteRecipes(favoriteRecipeViewState.favRecipes)
+            }
+        }
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+
+        recipe_loading_anim.isVisible(isLoading)
+
+    }
+
+    private fun displayFavoriteRecipes(favRecipes: LiveData<List<FavoriteRecipe>>) {
+
+        favRecipes.observe(viewLifecycleOwner,Observer{favoriteRecipeList->
+            if (favoriteRecipeList != null){
+                setUpRecyclerView(favoriteRecipeList)
+            }else{
+                showEmptyState()
+            }
+        })
+
+    }
+
+    private fun setUpRecyclerView(favoriteRecipeList: List<FavoriteRecipe>) {
+        showLoading(false)
+        favoriteRecipeRecyclerView.layoutManager = LinearLayoutManager(activity,LinearLayoutManager.VERTICAL,false)
+        favoriteRecipeRecyclerView.adapter = FavoriteRecipeAdapter(favoriteRecipeList,this)
+
+    }
+
+    private fun showEmptyState() {
+        showLoading(false)
+        no_fav_recipe.isVisible(true)
+    }
+
+    override fun favRecipeItemClicked(favRecipeId: Int) {
+        TODO("Not yet implemented")
     }
 
 }
